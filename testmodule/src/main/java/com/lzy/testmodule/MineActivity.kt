4af28b3a3c3bf79a966fac318d012ff9b5600631
Zhengyu Li <lizhengyu617@126.com>
2019-05-24 16:20:52 +0800

    kotlin 注解 使用kapt

--- testmodule/src/main/java/com/lzy/testmodule/MineActivity.kt
+++ testmodule/src/main/java/com/lzy/testmodule/MineActivity.kt
@@ -2,12 +2,14 @@ package com.lzy.testmodule
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.lzy.yrouter.annotation.Route
 
 /**
  * @author Lizhengyu
  * @description
  * @date 2019/5/24 13:44
  **/
+@Route("mine/mine")
 class MineActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
